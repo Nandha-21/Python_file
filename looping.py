c808cde1b155d8249c22688482fd2f01301a4f9c
Nandha-21 <nandhug2126@gmail.com>
2024-09-06 19:01:01 +0530

    add file

---
+++ looping.py
@@ -0,0 +1,20 @@
+#FOR LOOPING
+a=4
+for i in range(a,20,2):
+    print(i)
+#while LOOPING
+a=5
+while(a<10):
+    print(a)
+    a=a+1
+    #a+=1   INCREAMENT OPERATOR
+    #a-=1   DECREAMENT OPERATOR
+a=1
+b=10
+while (a<=10):
+    print(a,"x",b,"=",a*b)
+    a+=1
+a=1
+b=25
+for i in range (a,11):
+    print(i,"x",a,"=",b*i)
